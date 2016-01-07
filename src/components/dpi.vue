f550905d2cb177dd6b9806fda302fc0f15489e6d
phantomk <phantomk@foxmail.com>
2016-01-07 23:43:48 +0800

    update

---
+++ src/components/dpi.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="dpi">
+    <button type="button" name="button">1920x1080</button>
+    <button type="button" name="button">1366x768</button>
+  </div>
+</template>
+
+<script lang="coffee">
+
+</script>
+
+<style lang="stylus">
+</style>
